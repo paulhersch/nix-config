08513475d17b3d18e4a13c27e7362ab9c03ee617
paulhersch <74120050+paulhersch@users.noreply.github.com>
2022-07-04 21:27:36 +0200

    changed everblush build, now only copying neccessary files

--- ownPkgs/everblushgtk.nix
+++ ownPkgs/everblushgtk.nix
@@ -11,22 +11,13 @@ stdenv.mkDerivation {
 		rev = "main";
 		sha256 = "1fmjv898j10j64jrj1ddpp4xpf76awnvakjwmahqhs9wd114x3xs";
 	};
-#	nativeBuildInputs = with pkgs.nodePackages; [
-#		sass
-#	];
-#	preBuild = ''
-#		mkdir -p $out/share/themes/Everblush-gtk/gtk-3.0
-#	'';
-#	buildPhase = ''
-#		runHook preBuild
-#		sass $src/scss/gtk-3.0/gtk.scss $out/share/themes/Everblush-gtk/gtk-3.0/gtk.css
-#	'';
 	dontBuild = true;
 	preInstall = ''
 		mkdir -p $out/share/themes/Everblush-gtk
 	'';
 	installPhase = ''
 		runHook preInstall
-		cp -r $src/* $out/share/themes/Everblush-gtk/
+		install -m 0644 -v $src/index.theme $out/share/themes/Everblush-gtk/
+		cp -r $src/gtk-* $out/share/themes/Everblush-gtk/
 	'';
 }
