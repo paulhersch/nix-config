e9e5d74679a08643e980b58e62948aabda6775da
paulhersch <74120050+paulhersch@users.noreply.github.com>
2022-07-13 14:28:50 +0200

    trying out rxyhn colorscheme

--- video/theming/colors.nix
+++ video/theming/colors.nix
@@ -27,26 +27,51 @@ rec {
 	#	c15	= "586055";
 
 	#everblush
-		bg = "181f21";
-		fg = "dadada";
-		
-		dbg = "080f11";
-		lbg = "383f41";
+	#	bg = "181f21";
+	#	fg = "dadada";
+	#	
+	#	dbg = "080f11";
+	#	lbg = "383f41";
+	#
+	#	c0 = "454c4e";
+	#	c1 = "e06e6e";
+	#	c2 = "8ccf7e";
+	#	c3 = "e5c76b";
+	#	c4 = "67b0e8";
+	#	c5 = "c47fd5";
+	#	c6 = "6da4cd";
+	#	c7 = "b3b9b8";
+	#	c8 = "3b4244";
+	#	c9 = "ef7d7d";
+	#	c10 = "9bdead";
+	#	c11 = "f4d67a";
+	#	c12 = "6cb5ed";
+	#	c13 = "ce89df";
+	#	c14 = "67cbe7";
+	#	c15 = "bdc3c2";
 
-		c0 = "454c4e";
-		c1 = "e06e6e";
-		c2 = "8ccf7e";
-		c3 = "e5c76b";
-		c4 = "67b0e8";
-		c5 = "c47fd5";
-		c6 = "6da4cd";
-		c7 = "b3b9b8";
-		c8 = "3b4244";
-		c9 = "ef7d7d";
-		c10 = "9bdead";
-		c11 = "f4d67a";
-		c12 = "6cb5ed";
-		c13 = "ce89df";
-		c14 = "67cbe7";
-		c15 = "bdc3c2";
+	# aesthetic night with slightly shifted bg (rxyhn)
+		bg = "1c252c";
+		fg = "d9d7d6";
+
+		dbg = "0d181c";
+		lbg = "3c353c";
+
+		c0 = "1c252c";
+		c1 = "df5b61";
+		c2 = "78b892";
+		c3 = "de8f78";
+		c4 = "6791c9";
+		c5 = "bc83e3";
+		c6 = "67afc1";
+		c7 = "d9d7d6";
+
+		c8 = "484e5b";
+		c9 = "f16269";
+		c10 = "8cd7aa";
+		c11 = "e9967e";
+		c12 = "79aaeb";
+		c13 = "c488ec";
+		c14 = "7acfe4";
+		c15 = "e5e5e5";
 }
