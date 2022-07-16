6d76ca5f8c7303de2c7e93b7e1a1401f8e9188c7
paulhersch <74120050+paulhersch@users.noreply.github.com>
2022-07-16 18:28:16 +0200

    switched to sonokai maia typa colorscheme

--- video/theming/colors.nix
+++ video/theming/colors.nix
@@ -27,28 +27,28 @@ rec {
 	#	c15	= "586055";
 
 	#everblush
-		bg = "181f21";
-		fg = "dadada";
+	#	bg = "181f21";
+	#	fg = "dadada";
 		
-		dbg = "080f11";
-		lbg = "383f41";
+	#	dbg = "080f11";
+	#	lbg = "383f41";
 	
-		c0 = "454c4e";
-		c1 = "e06e6e";
-		c2 = "8ccf7e";
-		c3 = "e5c76b";
-		c4 = "67b0e8";
-		c5 = "c47fd5";
-		c6 = "6da4cd";
-		c7 = "b3b9b8";
-		c8 = "3b4244";
-		c9 = "ef7d7d";
-		c10 = "9bdead";
-		c11 = "f4d67a";
-		c12 = "6cb5ed";
-		c13 = "ce89df";
-		c14 = "67cbe7";
-		c15 = "bdc3c2";
+	#	c0 = "454c4e";
+	#	c1 = "e06e6e";
+	#	c2 = "8ccf7e";
+	#	c3 = "e5c76b";
+	#	c4 = "67b0e8";
+	#	c5 = "c47fd5";
+	#	c6 = "6da4cd";
+	#	c7 = "b3b9b8";
+	#	c8 = "3b4244";
+	#	c9 = "ef7d7d";
+	#	c10 = "9bdead";
+	#	c11 = "f4d67a";
+	#	c12 = "6cb5ed";
+	#	c13 = "ce89df";
+	#	c14 = "67cbe7";
+	#	c15 = "bdc3c2";
 
 	# aesthetic night with slightly shifted bg (rxyhn)
 	#	bg = "1c252c";
@@ -74,4 +74,30 @@ rec {
 	#	c13 = "c488ec";
 	#	c14 = "7acfe4";
 	#	c15 = "e5e5e5";
+	
+	# Sonokai Maia based colscheme
+
+		bg = "273136";
+		fg = "e1e2e3";
+
+		dbg = "1c1e1f";
+		lbg = "313b42";
+		
+		c0 = "273136";
+		c1 = "f76c7c";
+		c2 = "9cd57b";
+		c3 = "e3d367";
+		c4 = "78cee9";
+		c5 = "baa0f8";
+		c6 = "8ac1d2";
+		c7 = "55626d";
+		
+		c8 = "${lbg}";
+		c9 = "${c1}";
+		c10= "${c2}";
+		c11= "${c3}";
+		c12= "${c4}";
+		c13= "${c5}";
+		c14= "${c6}";
+		c15 = "82878b";
 }
