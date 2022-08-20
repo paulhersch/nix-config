ee3f2d5bb0083eff808ee54d27b2f6960aef0362
paulhersch <74120050+paulhersch@users.noreply.github.com>
2022-08-20 15:08:27 +0200

    kernel lts > latest

--- laptop.nix
+++ laptop.nix
@@ -1,16 +1,22 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+  networking.hostName = "snowflake";
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "sd_mod" "cryptd" ];
-  boot.initrd.kernelModules = [ "amdgpu"];
-  boot.kernelModules = [ "kvm-amd" ];
-  boot.kernelParams = [ "resume=UUID=6913a5e4-7c10-4a61-9fe9-d5b759e5c16e" ];
+  boot = {
+  	initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "sd_mod" "cryptd" ];
+	initrd.kernelModules = [ "amdgpu"];
+	kernelModules = [ "kvm-amd" ];
+	kernelPackages = pkgs.linuxPackages_latest;
+	kernelParams = [ "resume=UUID=6913a5e4-7c10-4a61-9fe9-d5b759e5c16e" ];
+	loader = {
+		efi.efiSysMountPoint = "/boot";
+  	};
+  };
   
   services.xserver.videoDrivers = [ "amdgpu" ];
+  
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/addcd413-7635-48ae-b091-4892545a0a5c";
       fsType = "btrfs";
@@ -35,8 +41,3 @@
 	device = "/dev/disk/by-uuid/9dc22222-57ab-4519-aca6-4bfc9a1e90c7";
 	preLVM = true;
     };
-  networking.hostName = "snowflake";
-  boot.loader = {
-	efi.efiSysMountPoint = "/boot";
-  };
-}
