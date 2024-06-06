02190cab9ba5aab3bf8ac20d83d63a6ee47e9510
paulhersch <74120050+paulhersch@users.noreply.github.com>
2024-06-06 14:19:19 +0200

    removed unnessecary stuff from local zsh settings

--- users/paul/default.nix
+++ users/paul/default.nix
@@ -124,21 +124,24 @@ in
 		gtk = import ./gtk.nix { inherit pkgs; };
 		xresources.extraConfig = import ./confs/xresources.nix { inherit theme; };
 		
-		home.file = {
-			".config/wezterm/wezterm.lua".text = import ./confs/wez.nix { inherit theme; };
-			".config/alacritty/alacritty.yml".text = import ./confs/alacritty.nix { inherit theme; };
-			".config/sway/config".text = import ./confs/sway/config.nix { inherit pkgs; inherit theme; };
-			".config/fontconfig/conf.d/99-alias-main.conf".text = import ./confs/fontconf.nix {};
-			".zshrc".text = ''
-				eval "$(direnv hook zsh)"
-				export ZK_NOTEBOOK_DIR=$HOME/Dokumente/Uni/zk
-			'';
-		};
+		home = {
+            file = {
+                ".config/wezterm/wezterm.lua".text = import ./confs/wez.nix { inherit theme; };
+                ".config/alacritty/alacritty.yml".text = import ./confs/alacritty.nix { inherit theme; };
+                ".config/sway/config".text = import ./confs/sway/config.nix { inherit pkgs; inherit theme; };
+                ".config/fontconfig/conf.d/99-alias-main.conf".text = import ./confs/fontconf.nix {};
+                ".zshenv".text = "";
+            };
+            sessionVariables = {
+                ZK_NOTEBOOK_DIR="$HOME/Dokumente/Uni/zk";
+            };
+        };
 
 		programs = {
 			autorandr = import ./autorandrhm.nix {};
 			direnv = {
 				enable = true;
+                enableZshIntegration = true;
 				nix-direnv.enable = true;
 			};
 			zathura = {
